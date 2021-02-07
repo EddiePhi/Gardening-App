24cbda35f2a59006bc5aa4651a9939ed21eb3c4b
Eddie Phimansone <ephimansone@gmail.com>
2021-02-07 13:30:18 -0500

    adjusted config, models, html, js, and apiRoutes folders,as well as server.js and package.json

---
+++ public/js/signup.js
@@ -0,0 +1,54 @@
+// Referencing Week 14 HW - Reverse-Engineering-Code
+
+$(document).ready(function() {
+  // Getting references to our form and input
+  var signUpForm = $("#login");
+  var emailInput = $("input#email-input");
+  var passwordInput = $("input#password-input");
+
+  // When the signup button is clicked, we validate the email and password are not blank
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
+    var userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim()
+    };
+
+    if (!userData.email || !userData.password) {
+      return;
+    }
+    // If we have an email and password, run the signUpUser function
+    signUpUser(userData.email, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
+  });
+
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function signUpUser(email, password) {
+    $.post("/api/signup", {
+      email: email,
+      password: password
+    })
+      .then(function(data) {
+        window.location.replace("/home");
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+
+  // https://developers.google.com/identity/sign-in/web/sign-in
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+});
--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 
 const express = require("express");
 require("dotenv").config();
+const passport = require("./config/passport");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -24,6 +25,10 @@ const db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // ================================================================================
 // ROUTER
